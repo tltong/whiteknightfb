51aadc0d0d5ce1149914445850aaceb074c57a64
tltong <tltong@yahoo.co.uk>
2017-12-25 06:24:59 +0000

    home.ts and home.html

--- pages/home/home.ts
+++ pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { Facebook } from '@ionic-native/facebook';
 import { NavController } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
+import firebase from 'firebase';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  userProfile: any = null;
+  constructor(public navCtrl: NavController, private facebook: Facebook) {
 
   }
+
+  facebookLogin(){
+    this.facebook.login(['email']).then( (response) => {
+        const facebookCredential = firebase.auth.FacebookAuthProvider.credential(response.authResponse.accessToken);
+
+        firebase.auth().signInWithCredential(facebookCredential).then((success) => {
+            console.log("Firebase success: " + JSON.stringify(success));
+            this.userProfile = success;
+        })
+        .catch((error) => {
+            console.log("Firebase failure: " + JSON.stringify(error));
+        });
+
+    }).catch((error) => { console.log(error) });
+  }
+
+
+
 
 }
