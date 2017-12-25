dc9eb6c1a8adf2d1b9912e08124715687fa6dced
tltong <tltong@yahoo.co.uk>
2017-12-25 06:07:50 +0000

    app.module.ts

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,9 +3,22 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { Facebook } from '@ionic-native/facebook'
+import { AngularFireAuth } from 'angularfire2/auth';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireModule } from 'angularfire2';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBSgJbI0E6V7b-nXgZB2y7DrTbX5DHPRCs",
+    authDomain: "whiteknightfbtest.firebaseapp.com",
+    databaseURL: "https://whiteknightfbtest.firebaseio.com",
+    projectId: "whiteknightfbtest",
+    storageBucket: "whiteknightfbtest.appspot.com",
+    messagingSenderId: "383610767012"
+};
 
 @NgModule({
   declarations: [
@@ -14,7 +27,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -24,6 +39,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    AngularFireAuth,
+    Facebook,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
